874729d3e357c979f74fbff1cbcc019a38f1a8ad
Winotai Saiyuenyong <saiyuenyong_w@silpakorn.edu>
2023-01-15 23:24:34 +0700

    abc

--- lib/main.dart
+++ lib/main.dart
@@ -80,154 +80,170 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          //mainAxisAlignment: MainAxisAlignment.start,
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
           children: [
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: OutlinedButton(
-                  onPressed: () {
-                    //todo:
-                  },
-                  child: Text('January'),
+            Row(
+              //mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        //todo:
+                      },
+                      child: Text('January'),
+                    ),
+                  ),
                 ),
-              ),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        //todo:
+                      },
+                      child: Text('February'),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        //todo:
+                      },
+                      child: Text('March'),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: OutlinedButton(
-                  onPressed: () {
-                    //todo:
-                  },
-                  child: Text('February'),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        //todo:
+                      },
+                      child: Text('April'),
+                    ),
+                  ),
                 ),
-              ),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        //todo:
+                      },
+                      child: Text('May'),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        //todo:
+                      },
+                      child: Text('June'),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: OutlinedButton(
-                  onPressed: () {
-                    //todo:
-                  },
-                  child: Text('March'),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        //todo:
+                      },
+                      child: Text('July'),
+                    ),
+                  ),
                 ),
-              ),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        //todo:
+                      },
+                      child: Text('August'),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        //todo:
+                      },
+                      child: Text('September'),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: OutlinedButton(
-                  onPressed: () {
-                    //todo:
-                  },
-                  child: Text('April'),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        //todo:
+                      },
+                      child: Text('October'),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: OutlinedButton(
-                  onPressed: () {
-                    //todo:
-                  },
-                  child: Text('May'),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        //todo:
+                      },
+                      child: Text('November'),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: OutlinedButton(
-                  onPressed: () {
-                    //todo:
-                  },
-                  child: Text('June'),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        //todo:
+                      },
+                      child: Text('December'),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: OutlinedButton(
-                  onPressed: () {
-                    //todo:
-                  },
-                  child: Text('July'),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: OutlinedButton(
-                  onPressed: () {
-                    //todo:
-                  },
-                  child: Text('August'),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: OutlinedButton(
-                  onPressed: () {
-                    //todo:
-                  },
-                  child: Text('September'),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: OutlinedButton(
-                  onPressed: () {
-                    //todo:
-                  },
-                  child: Text('October'),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: OutlinedButton(
-                  onPressed: () {
-                    //todo:
-                  },
-                  child: Text('November'),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: OutlinedButton(
-                  onPressed: () {
-                    //todo:
-                  },
-                  child: Text('December'),
-                ),
-              ),
+              ],
             ),
           ],
         ),
