1b39e3b3730411d86bf9b00d32dcb330a32a614c
Winotai Saiyuenyong <saiyuenyong_w@silpakorn.edu>
2023-01-15 12:08:31 +0700

    abc

--- lib/page/calendar_2023.dart
+++ lib/page/calendar_2023.dart
@@ -1,68 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
-//import 'package:midterm_620710131/page/calendar_2023.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CALENDAR 2023',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.indigo,
-      ),
-      home: const MyHomePage(title: 'abc'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class GamePage extends StatefulWidget {
+  const GamePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<GamePage> createState() => _GamePagesState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _GamePagesState extends State<GamePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,8 +17,8 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             Container(
               decoration:
-              BoxDecoration(color: Colors.white, shape: BoxShape.circle),
-              //decoration : TextDecoration.underline
+                  BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+                //decoration : TextDecoration.underline
             ),
             const SizedBox(width: 10),
             const Text('←    CALENDAR 2023'),
